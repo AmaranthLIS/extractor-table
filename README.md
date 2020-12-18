87ffc6d400c5e3ac597cc19cce1f23e7840b169c
lis <foxigs@gmail.com>
2020-12-18 23:42:31 +0400

    upload project

---
+++ README.md
@@ -0,0 +1,12 @@
+## Feature jdbc-hikari documentation
+
+- [Micronaut Hikari JDBC Connection Pool documentation](https://micronaut-projects.github.io/micronaut-sql/latest/guide/index.html#jdbc)
+
+## Feature sql-jdbi documentation
+
+- [Micronaut Jdbi documentation](https://micronaut-projects.github.io/micronaut-sql/latest/guide/index.html#jdbi)
+
+## Feature testcontainers documentation
+
+- [https://www.testcontainers.org/](https://www.testcontainers.org/)
+
---
+++ src/main/kotlin/ge/lis/tool/ExtractorTableCommand.kt
@@ -0,0 +1,30 @@
+package ge.lis.tool
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "extractor-table", description = ["..."],
+        mixinStandardHelpOptions = true)
+class ExtractorTableCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(ExtractorTableCommand::class.java, *args)
+        }
+    }
+}
